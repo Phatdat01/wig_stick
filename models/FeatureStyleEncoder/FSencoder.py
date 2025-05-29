b17b8387838a72459c4afcc8c1a5647017be8d16
Phatdat01 <phatdatlht2001@gmail.com>
2025-05-29 10:50:44 +0700

    fix: fix fsender

--- models/FeatureStyleEncoder/FSencoder.py
+++ models/FeatureStyleEncoder/FSencoder.py
@@ -9,33 +9,62 @@ import torch.utils.data as data
 import yaml
 import sys
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, current_dir)
-
 from PIL import Image
 from tqdm import tqdm
 from torchvision import transforms, utils
 
-from trainer import *
+# Setup paths and environment
+current_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, current_dir)
 
 torch.backends.cudnn.enabled = True
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = True
-# torch.autograd.set_detect_anomaly(True)
 Image.MAX_IMAGE_PIXELS = None
 
-opts = Namespace(config='001', pretrained_model_path='pretrained_models/FeatureStyleEncoder/143_enc.pth', stylegan_model_path=f'pretrained_models/FeatureStyleEncoder/psp_ffhq_encode.pt', arcface_model_path=f'pretrained_models/FeatureStyleEncoder/backbone.pth', parsing_model_path=f'pretrained_models/FeatureStyleEncoder/79999_iter.pth', log_path='./logs/', resume=False, checkpoint='', checkpoint_noiser='', multigpu=False, input_path='./test/', save_path='./')
+# Set training options
+opts = Namespace(
+    config='001',
+    pretrained_model_path='pretrained_models/FeatureStyleEncoder/143_enc.pth',
+    stylegan_model_path='pretrained_models/FeatureStyleEncoder/psp_ffhq_encode.pt',
+    arcface_model_path='pretrained_models/FeatureStyleEncoder/backbone.pth',
+    parsing_model_path='pretrained_models/FeatureStyleEncoder/79999_iter.pth',
+    log_path='./logs/',
+    resume=False,
+    checkpoint='',
+    checkpoint_noiser='',
+    multigpu=False,
+    input_path='./test/',
+    save_path='./'
+)
 
-config = yaml.load(open(f'{current_dir}/configs/' + opts.config + '.yaml', 'r'), Loader=yaml.FullLoader)
+# Load configuration
+config_path = os.path.join(current_dir, 'configs', f'{opts.config}.yaml')
+config = yaml.load(open(config_path, 'r'), Loader=yaml.FullLoader)
 
-def get_trainer(device):
-    # Initialize trainer
+from trainer import Trainer
+
+def get_trainer(device='cuda'):
+    # Set the desired device in opts for downstream modules
+    opts.device = device
+
+    # Initialize Trainer object with config and options
     trainer = Trainer(config, opts)
-    trainer.initialize(opts.stylegan_model_path, opts.arcface_model_path, opts.parsing_model_path)  
+
+    # Initialize with paths to pretrained models
+    trainer.initialize(
+        stylegan_path=opts.stylegan_model_path,
+        arcface_path=opts.arcface_model_path,
+        parsing_path=opts.parsing_model_path
+    )
+
+    # Move entire trainer to the selected device
     trainer.to(device)
 
-    # state_dict = torch.load(opts.pretrained_model_path)#os.path.join(opts.log_path, opts.config + '/checkpoint.pth'))
-    trainer.enc.load_state_dict(torch.load(opts.pretrained_model_path))
+    # Load pretrained encoder weights (FeatureStyleEncoder)
+    print(f"Loading encoder weights from {opts.pretrained_model_path}")
+    enc_weights = torch.load(opts.pretrained_model_path, map_location=device)
+    trainer.enc.load_state_dict(enc_weights)
     trainer.enc.eval()
-    
+
     return trainer
